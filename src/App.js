2778de0188f7388e536b3dbaa8a932dca48d9e8b
frankling <3036431296@qq.com>
2023-04-01 22:54:57 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,202 @@
+// Desc: This is the main component of the application.
+// Author: hyfrankl
+
+
+// Import dependencies
+import React, { useState } from "react";
+import Form from "./components/Form";
+import { Configuration, OpenAIApi } from "openai";
+import { Histogram } from "./components/Histogram";
+
+
+function App(props) {
+	// Create state variables for response, submitView, jaccard, query, api, inputText
+	// response: the response from the OpenAI API rendered with highlighting
+	const [response, setResponse] = useState(`Hello from ChatGPT!`);
+	// submitView: a boolean to determine whether to show the form or the response
+	const [submitView, setSubmitView] = useState(true);
+	// jaccard: the Jaccard similarity between the query and the response
+	const [jaccard, setJaccard] = useState(-1);
+	// query: the query that the user inputs
+	const [query, setQuery] = useState("");
+	// api: the API key that the user inputs
+    const [api, setAPI] = useState("");
+	// inputText: the text that contains the orignal response
+	const [inputText , setInputText] = useState("");
+
+	// Create the instructions text for ChatGPT to create a summary
+	const text = `
+		Please do the task step-by-step:
+		1. summarize the following text.
+		2. extract keywords or important concepts from the summary or the original text 
+		and output those words in a new paragraph at the end of the respoonse where 
+		each word is separated by a comma after "Keywords:".
+
+	`;
+
+	// Create a function to generate a random pastel color
+	// to avoid any dark colors that may be hard to read
+	const GetRandomPastelColor = () => {
+		const h = Math.floor(Math.random() * 360);
+		return `hsl(${h}deg, 100%, 90%)`;
+	};
+
+	// Create a function to fetch the response from the OpenAI API
+	// using the input text and the API key
+	// and return a promise that resolves to the response
+	const FetchResponse = async (input, api_key) => {
+		const config = new Configuration({ apiKey: api_key });
+		const openai = new OpenAIApi(config);
+		
+		return openai.createChatCompletion({
+			model: "gpt-3.5-turbo",
+			messages: [{role: 'system', content: text}, { role: "user", content: input }],
+		});
+	};
+
+	// Create a function to clear the input text, query, and response in the form
+	// and reset the jaccard similarity to -1 as well as the histogram
+	const ClearAll = () => {
+        if (api !== "") {
+			setAPI("");
+		}
+		if (query !== "") {
+			setQuery("");
+		}
+		setJaccard(-1);
+		setInputText("");
+		setResponse(`Hello from ChatGPT!`);
+    };
+
+	// Create a function to handle highlighting the keywords
+	// and return the highlighted text
+	// implemented with regex and the Jaccard similarity
+	const HighlightKeywords = (keywords, text, query) => {
+		let result = text;
+		let q = query;
+		let jaccardSim = 0;
+		keywords.sort((a, b) => (b.length - a.length || a.localeCompare(b)));
+
+		for (let keyword of keywords) {
+			let color = GetRandomPastelColor();
+			let reg_word = new RegExp(`\\b${keyword}\\b`, 'ig');
+			if (result.match(reg_word) && q.match(reg_word)) {
+				jaccardSim += 1;
+			}
+			result = result.replaceAll(reg_word, `<mark style="background: ${color}!important">$&</mark>`);
+			q = q.replaceAll(reg_word, `<mark style="background: ${color}!important">$&</mark>`);
+			
+		}
+		jaccardSim /= keywords.length;
+		setJaccard(jaccardSim.toFixed(3));
+		return [result, q];
+	};
+
+	// Create a function to display the response and
+	// set state variables for response, query, and inputText.
+	const DisplayResponse = async (input, api) => {
+		const answer = await FetchResponse(input, api);
+		let result = answer.data.choices[0].message.content;
+		result = result.substring(0, result.length - 1);
+		let paragraphs = result.split('\n\n').map((paragraph) => { return paragraph.trim().trimEnd('.'); })
+		const keywords = paragraphs[paragraphs.length - 1]
+			.split(':')[1].split(',')
+			.map((keyword) => { return keyword.trim(); });
+		let [r, q] = HighlightKeywords(keywords, result, input);
+
+		setResponse(<div dangerouslySetInnerHTML={{ __html: r }} />);
+		setQuery(<div dangerouslySetInnerHTML={{ __html: q }} />);
+		const words = result.toLowerCase().split(/\s+/);
+        const wordLengthsCount = words.map(word => word.length)
+		setInputText(wordLengthsCount);
+	}
+
+	// It handles the response to sumbit the form.
+	// This function will call other functoins to handle
+	// highlighting the keywords and fetching the response.
+	const GetResponse = async () => {
+		setJaccard(-1);
+		setInputText("");
+		try {
+			if (query.length !== 0 && api.length !== 0) {
+				setSubmitView(false);
+				setResponse(`Loading...`);
+				await DisplayResponse(query, api);
+			} else {
+				// Reset query to deal with empty query
+				if (query.length !== 0) {
+					setQuery(query);
+				}
+				// Reset api to deal with empty api
+				if (api.length !== 0) {
+					setAPI(api);
+				}
+				// Display error message
+				setResponse(<mark>Please enter your API key and text to summarize.</mark>);
+			}
+		} catch (error) {
+			// Display error message
+			setResponse(<mark>{error.message}</mark>);
+		}
+	}
+
+	// Create a function to show the form view
+	const formView = (
+		<div className="column todoapp stack-large">
+			<h1>Simple Summarizer </h1>
+			<h2 className="label-wrapper">
+                <label htmlFor="password-input" className="label__lg">
+                    What needs to be done? 
+					({ api !== ""?  "Key Stored":
+						<a href="https://platform.openai.com/account/api-keys">Fetch API Key</a>})
+                </label>
+            </h2>
+			<Form setQuery={setQuery} setAPI={setAPI} onSubmit={GetResponse} clearAll={ClearAll}/>
+			<div align='center'>
+				<a href="https://github.com/frankling2020/react-chatgpt">Created by frankling 🐲</a>
+			</div>
+		</div>
+	);
+
+	// Create a function to show the input text view
+	const resultView = (
+		<div className="column todoapp stack-large scrollable">
+			<h1>Original Text</h1>
+			<button type="reset" className="btn btn__secondary btn__sm"
+				onClick={(e) => { e.preventDefault(); setQuery(""); setSubmitView(true); }}>
+				&#8701; Back
+			</button>
+			<div className="css-fix"> {query} </div>
+		</div>
+	);
+
+	// Create a function to show the summary view with summary, Jaccard similarity and histogram
+	const jaccardView = (<div><em>Jaccard Similarity: { jaccard }</em></div>);
+	const summaryView = (
+		<div className="column todoapp stack-large scrollable">
+			<h1>Summary and Keywords</h1>
+			{jaccard !== -1 && jaccardView}
+			<div className="css-fix"> 
+				{response} 
+			</div>
+			{inputText.length !== 0 && 
+				<div align='center'> 
+					<Histogram data={inputText} width={360} height={360} />  
+				</div>}
+		</div>
+	);
+
+	// Return the form view if submitView is true, otherwise return the result view
+	return (
+		<div>
+			<div className="row">
+				{ submitView? formView : resultView } 
+				{ summaryView }
+			</div>
+		</div>
+
+	);
+}
+
+
+export default App;
---
+++ src/components/Histogram.js
@@ -0,0 +1,101 @@
+// Based on codes from https://www.react-graph-gallery.com/histogram
+// it is a histogram that shows the distribution of the length of words in the text
+
+import { useEffect, useMemo, useRef } from "react";
+import * as d3 from 'd3';
+import React from "react";
+
+// define constants for the visualization
+const MARGIN = { top: 30, right: 30, bottom: 40, left: 50 };
+const BUCKET_NUMBER = 5;
+const BUCKET_PADDING = 0;
+
+// define the component of Histogram
+export const Histogram = ({ width, height, data }) => {
+    const axesRef = useRef(null);
+
+    // compute the width and height of the bounds
+    const boundsWidth = width - MARGIN.right - MARGIN.left;
+    const boundsHeight = height - MARGIN.top - MARGIN.bottom;
+
+    // compute the xScale, buckets, yScale
+    const xScale = useMemo(() => {
+        return d3
+            .scaleLinear()
+            .domain([0, 30])            
+            .range([10, boundsWidth]);
+    }, [boundsWidth]);
+
+    const buckets = useMemo(() => {
+        const bucketGenerator = d3
+            .bin()
+            .value((d) => d)
+            .domain(xScale.domain())
+            .thresholds(xScale.ticks(BUCKET_NUMBER))
+        return bucketGenerator(data);
+    }, [xScale, data]);
+
+    const yScale = useMemo(() => {
+        const max = Math.max(...buckets.map((bucket) => bucket?.length));
+        return d3.scaleLinear().range([boundsHeight, 0]).domain([0, max]).nice();
+    }, [boundsHeight, buckets]);
+
+    // Render the X axis using d3.js, not react
+    useEffect(() => {
+        const svgElement = d3.select(axesRef.current);
+        svgElement.selectAll("*").remove();
+        
+        // add the x axis
+        const xAxisGenerator = d3.axisBottom(xScale);
+        svgElement
+            .append("g")
+            .attr("transform", "translate(0," + boundsHeight + ")")
+            .call(xAxisGenerator);
+
+        // add the y axis
+        const yAxisGenerator = d3.axisLeft(yScale);
+        svgElement.append("g").call(yAxisGenerator);
+        
+        // add the title
+        svgElement.append("text")
+            .attr("x", width / 2 - 50)
+            .attr("y", -10)
+            .style("text-anchor", "middle")
+            .style("font-size", "12px")
+            .style("font-weight", "bold")
+            .text("Word Length Count in Summary")
+    }, [xScale, yScale, boundsHeight, width]);
+
+    // create the rectangles for the histogram
+    const allRects = buckets.map((bucket, i) => {
+        return (
+            <rect
+                key={i}
+                fill="#69b3a2"
+                x={xScale(bucket.x0) + BUCKET_PADDING / 2}
+                width={xScale(bucket.x1) - xScale(bucket.x0) - BUCKET_PADDING}
+                y={yScale(bucket.length)}
+                height={boundsHeight - yScale(bucket.length)}
+            />
+        );
+    });
+
+    // return the svg to render the histogram
+    return (
+        <svg width={width} height={height}>
+            <g
+                width={boundsWidth}
+                height={boundsHeight}
+                transform={`translate(${[MARGIN.left, MARGIN.top].join(",")})`}
+            >
+                {allRects}
+            </g>
+            <g
+                width={boundsWidth}
+                height={boundsHeight}
+                ref={axesRef}
+                transform={`translate(${[MARGIN.left, MARGIN.top].join(",")})`}
+            />
+        </svg>
+    );
+};
